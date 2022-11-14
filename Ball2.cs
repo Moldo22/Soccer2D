9d1c2d28059cf008f8730f3a31cb6ebd45af9f75
Moldovan Alex <115102799+Moldo22@users.noreply.github.com>
2022-11-14 09:51:11 +0200

    Create Ball2.cs

---
+++ Ball2.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random=System.Random;
+
+
+public class Ball2 : MonoBehaviour
+{
+    [SerializeField] Rigidbody2D rb;
+    Vector2 check;
+
+    public void getPushed(Vector2 dir,float force){
+        rb.AddForce(dir*force);
+    }
+
+}
